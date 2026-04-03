3eec2694a0ce6531b79b5fb49cda999374236d10
Clelia (Astra) Bertelli <clelia@runllama.ai>
2026-04-03 13:28:21 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+import {
+  asBooleanArray,
+  asFloatArray,
+  asIntArray,
+  asStringArray,
+  DataType,
+  readCsv,
+  type ColumnData,
+} from "@cle-does-things/sunbears";
+import { AlignmentEnum, AsciiTable3 } from "ascii-table3";
+
+function colToArray(
+  col: ColumnData,
+  dt: DataType,
+): string[] | number[] | boolean[] {
+  switch (dt) {
+    case DataType.String:
+      return asStringArray(col)!;
+    case DataType.Float:
+      return asFloatArray(col)!;
+    case DataType.Integer:
+      return asIntArray(col)!;
+    case DataType.Boolean:
+      return asBooleanArray(col)!;
+    default:
+      throw new Error(`Unexpected data type: ${dt}`);
+  }
+}
+
+function transformColumns(cols: (string[] | boolean[] | number[])[]) {
+  let i = 0;
+  const matrix = [];
+  while (i < cols[0]!.length) {
+    const row = [];
+    for (const c of cols) {
+      row.push(c[i]);
+    }
+    matrix.push(row);
+    i++;
+  }
+  return matrix;
+}
+
+function main(): void {
+  const firstArg = process.argv.at(2);
+  if (firstArg && (firstArg === "--help" || firstArg === "-h")) {
+    console.log(
+      "Read a CSV file and output it as a ASCII table to terminal.\n\nArguments:\n  PATH: the path to the CSV file to read\n\nOptions:\n  --help/-h: Print the help message.",
+    );
+    return;
+  }
+  if (!firstArg) {
+    console.error("You should provide a CSV file path");
+    return;
+  }
+  const df = readCsv(firstArg);
+  const cols = Object.keys(df.columns);
+  const colData = Object.values(df.columns);
+  const transformCols = [];
+  let i = 0;
+  while (i < colData.length) {
+    const dt = df.colDtype(cols[i]!)!;
+    const arr = colToArray(colData[i]!, dt);
+    transformCols.push(arr);
+    i++;
+  }
+  const rowMatrix = transformColumns(transformCols);
+  const table = new AsciiTable3(firstArg)
+    .setHeading(...cols)
+    .setAlign(cols.length, AlignmentEnum.CENTER)
+    .addRowMatrix(rowMatrix);
+  console.log(table.toString());
+}
+
+main();
